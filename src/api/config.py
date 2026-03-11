0e4fa578c5de6cd03b00988b2c971c84567318f8
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2026-03-11 23:00:53 +0100

    feat: Main API files

---
+++ src/api/config.py
@@ -0,0 +1,19 @@
+from sanic.config import Config
+
+
+class AppConfig(Config):
+    API_VERSION = "1.1.0"
+
+    OAS = True
+    OAS_UI_DEFAULT = "scalar"
+    OAS_UI_SCALAR = True
+    OAS_PATH_TO_SCALAR_HTML = "scalar.html"
+    OAS_UI_REDOC = False
+    OAS_UI_SWAGGER = False
+    OAS_URI_TO_JSON = "/openapi.json"
+    OAS_URL_PREFIX = "/"
+
+    # CORS_ORIGINS = "*"
+    # CORS_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+
+    FALLBACK_ERROR_FORMAT = "json"
